5ac8a78d75e946731b583847736c6038d1021a1d
Trevor Robinson <trevor@scurrilous.com>
2020-01-24 19:19:50 -0600

    Add mutation resolver utilities

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,6 +1,7 @@
 export * from './api';
 export * from './ColumnRestriction';
 export * from './JoinSpec';
+export * from './mutationUtil';
 export { ConnectionVisitors } from './ConnectionVisitors';
 export { EdgesVisitors } from './EdgesVisitors';
 export { PageInfoVisitors } from './PageInfoVisitors';
---
+++ src/resolver/mutationUtil.ts
@@ -0,0 +1,14 @@
+import mapObject from 'map-obj';
+import { snakeCase } from 'snake-case';
+
+export function hasDefinedElement(arr: any[]): boolean {
+  return arr.some(v => v !== undefined);
+}
+
+export function hasDefinedValue(obj: { [s: string]: any } | ArrayLike<any>): boolean {
+  return hasDefinedElement(Object.values(obj));
+}
+
+export function snakeCaseKeys(obj: object): object {
+  return mapObject(obj, (key, value) => [snakeCase(String(key)), value], { deep: true });
+}
