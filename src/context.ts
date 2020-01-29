dff7c974ccc8472df89094e02e313b24a750d6e9
Trevor Robinson <trevor@scurrilous.com>
2020-01-28 22:11:58 -0600

    Add createContext

--- src/context.ts
+++ src/context.ts
@@ -1,8 +1,20 @@
 import Knex from 'knex';
-import { SqlExecutor, SqlResolverFactory } from './resolver';
+import { createFactory, getDefaultSqlExecutor, SqlExecutor, SqlResolverFactory, SqlResolverOptions } from './resolver';
 
 export interface SqlResolverContext {
   knex: Knex;
   sqlExecutor: SqlExecutor;
   resolverFactory: SqlResolverFactory;
+  extend<Props extends {}>(props: Props): this & Props;
+}
+
+export function createContext(knex: Knex, defaultOptions?: Partial<SqlResolverOptions>): SqlResolverContext {
+  return {
+    knex,
+    sqlExecutor: getDefaultSqlExecutor(),
+    resolverFactory: createFactory(knex, defaultOptions),
+    extend(props) {
+      return Object.assign(this, props);
+    }
+  };
 }
