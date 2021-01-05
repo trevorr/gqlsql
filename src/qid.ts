d351ac210c5df5c708199b55e6a24f52d93cb910
Trevor Robinson <trevor@scurrilous.com>
2021-01-04 22:15:30 -0600

    Support qualified WKIDs

--- src/qid.ts
+++ src/qid.ts
@@ -1,15 +1,30 @@
 import { isTableMetadata, TableMetadata, TypeMetadata } from './meta';
 import { SqlFieldResolver, SqlQueryResolver } from './resolver';
 
-export function joinQid(randomId: string, tableId: string | undefined): string {
-  return tableId ? `${tableId}_${randomId}` : randomId;
+export function joinQrid(rid: string, tableId: string | undefined): string {
+  return tableId ? `${tableId}_${rid}` : rid;
+}
+
+export function joinQwkid(wkid: string, tableId: string | undefined): string {
+  return tableId ? `${tableId}:${wkid}` : wkid;
+}
+
+export function qualifyXid(xid: string, meta: TableMetadata): string {
+  if (meta.randomIdColumn) {
+    return joinQrid(xid, meta.tableId);
+  }
+  if (meta.wellKnownIdColumn) {
+    return joinQwkid(xid, meta.tableId);
+  }
+  throw new Error(`No external ID defined for ${meta.typeName}`);
 }
 
 export function splitQid(qid: string): [string, string?] {
-  const parts = qid.split('_', 2);
+  const parts = qid.split(/_|:/, 2);
   return parts.length > 1 ? [parts[1], parts[0]] : [parts[0]];
 }
 
+/** @deprecated */
 export function getRidFromQid(qid: string, meta?: TableMetadata): string {
   const [randomId, tableId] = splitQid(qid);
   if (tableId && meta && meta.tableId && tableId !== meta.tableId) {
@@ -18,8 +33,16 @@ export function getRidFromQid(qid: string, meta?: TableMetadata): string {
   return randomId;
 }
 
+export function getXidFromQid(qid: string, meta?: TableMetadata): string {
+  const [externalId, tableId] = splitQid(qid);
+  if (tableId && meta && meta.tableId && tableId !== meta.tableId) {
+    throw new Error(`Unexpected prefix for ${meta.typeName} ID "${qid}"`);
+  }
+  return externalId;
+}
+
 export function resolveQid(qid: string, meta: TypeMetadata): [string, TableMetadata] {
-  const [randomId, tableId] = splitQid(qid);
+  const [externalId, tableId] = splitQid(qid);
   let tableMeta;
   if (isTableMetadata(meta)) {
     if (tableId && meta.tableId && tableId !== meta.tableId) {
@@ -34,32 +57,36 @@ export function resolveQid(qid: string, meta: TypeMetadata): [string, TableMetad
       throw new Error(`Unknown prefix "${tableId}" in ${meta.typeName} ID "${qid}"`);
     }
   }
-  return [randomId, tableMeta];
+  return [externalId, tableMeta];
 }
 
 export function addQidField<T extends SqlFieldResolver>(resolver: T, field: string, meta: TableMetadata): T;
 export function addQidField<T extends SqlQueryResolver>(resolver: T, field: string, meta: TypeMetadata): T;
 export function addQidField<T extends SqlFieldResolver>(resolver: T, field: string, meta: TypeMetadata): T {
   if (isTableMetadata(meta)) {
-    if (!meta.randomIdColumn) {
-      throw new Error(`No random ID defined for ${meta.typeName}`);
+    if (meta.randomIdColumn) {
+      resolver.addColumnField(field, meta.randomIdColumn, meta.tableName, rid => joinQrid(rid, meta.tableId));
+    } else if (meta.wellKnownIdColumn) {
+      resolver.addColumnField(field, meta.wellKnownIdColumn, meta.tableName, wkid => joinQwkid(wkid, meta.tableId));
+    } else {
+      throw new Error(`No external ID defined for ${meta.typeName}`);
     }
-    resolver.addColumnField(field, meta.randomIdColumn, meta.tableName, rid => joinQid(rid, meta.tableId));
   } else {
     const queryResolver = (resolver as SqlFieldResolver) as SqlQueryResolver;
     const metas = Object.values(meta.tableIds).filter(meta => queryResolver.hasTable(meta.tableName));
-    const noRidMeta = metas.find(meta => !meta.randomIdColumn);
+    const noRidMeta = metas.find(meta => !meta.randomIdColumn && !meta.wellKnownIdColumn);
     if (noRidMeta) {
-      throw new Error(`No random ID defined for ${noRidMeta.typeName}`);
+      throw new Error(`No external ID defined for ${noRidMeta.typeName}`);
     }
-    const ridColumn = queryResolver.addCoalesceExpression(
-      metas.map(meta => [meta.tableName, meta.randomIdColumn!]),
-      'rid'
+    const xidColumn = queryResolver.addCoalesceExpression(
+      metas.map(meta => [meta.tableName, meta.randomIdColumn || meta.wellKnownIdColumn!]),
+      'xid'
     );
     resolver.addDerivedField(field, row => {
+      const xid = row[xidColumn];
       const typeName = queryResolver.getTypeNameFromRow(row);
       const actualMeta = (meta.objectTypes || metas).find(meta => meta.typeName === typeName);
-      return joinQid(row[ridColumn], actualMeta?.tableId);
+      return actualMeta ? qualifyXid(xid, actualMeta) : xid;
     });
   }
   return resolver;
